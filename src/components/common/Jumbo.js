3473698c699bfdea2e2c4ffb3a24dad4c29576f4
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-12 14:48:48 +0100

    Style generic jumbo

--- src/components/common/Jumbo.js
+++ src/components/common/Jumbo.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import world from '../../images/undraw_world.svg';
 
 const Jumbo = (props) => {
   const { name, cases } = props
   
   return (
-    <div>
-      <p>Jumbo</p>
-      <p>{name}</p>
-      <p>{cases}</p>
+    <div className="jumbo d-flex">
+      <div className="icon-div w-50">
+        <box-icon color="#7B2C4B" size="6rem" name='world'></box-icon>
+      </div>
+      <div className="w-50 jumbo-title">
+        <div>{name}</div>
+        <div>{cases}</div>
+      </div>
     </div>
   );
 }
