07a36e6c7b1b4b6efe9148a2a1651fc094e9ff7f
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-10-12 11:04:43 +0100

    Style Header component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import 'boxicons';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
   const { year, title } = props
   return (
-    <div className="d-flex">
-      <div><box-icon type='solid' name='chevron-left'></box-icon></div>
-      <p>{year}</p>
-      <div>{title}</div>
-      <div><box-icon name='microphone'></box-icon></div>
-      <div><box-icon name='cog'></box-icon></div>
+    <div className="d-flex header px-2 py-2">
+      <div className="d-flex">
+        <Link to="/">
+        <box-icon color="white" type='solid' name='chevron-left'></box-icon>
+        </Link>
+        <div className="text-white">{year}</div>
+      </div>
+      <div className="text-white">{title}</div>
+      <div className="d-flex">
+        <div className="me-4"><box-icon color="white" name='microphone'></box-icon></div>
+        <div><box-icon color="white" name='cog'></box-icon></div>
+      </div>
     </div>
   );
 }
